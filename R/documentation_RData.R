0e07b7edffea96072e068bac59dfdb577c321c2a
ahart1 <arhart555@gmail.com>
2020-08-26 09:58:39 -0400

    Add documentation for package RData sets

---
+++ R/documentation_RData.R
@@ -0,0 +1,98 @@
+# Dataset documentation script
+
+#' Raster data to display the Commercial Fisheries Icon for Herring, Mackerel, and Lobster
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Commercial_Fisheries_Herring_Mackerel_Lobster")), layer=1, values=TRUE) 
+"Icon_Commercial_Fisheries_Herring_Mackerel_Lobster"
+
+#' Raster data to display the Environmental Considerations Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Environmental_Considerations")), layer=1, values=TRUE)
+"Icon_Environmental_Considerations"
+
+#' Raster data to display the Groundfish Fishery Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Groundfish_Fishery")), layer=1, values=TRUE)
+"Icon_Groundfish_Fishery"
+
+#' Raster data to display the Groundfish Species Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Groundfish_Species")), layer=1, values=TRUE)
+"Icon_Groundfish_Species"
+
+#' Raster data to display the Herring Fishery Icon Option 1
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Herring_Fishery_Option1")), layer=1, values=TRUE)
+"Icon_Herring_Fishery_Option1"
+
+#' Raster data to display the Herring Fishery Icon Option 2
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Herring_Fishery_Option2")), layer=1, values=TRUE)
+"Icon_Herring_Fishery_Option2"
+
+#' Raster data to display the Lobster Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Lobster")), layer=1, values=TRUE)
+"Icon_Lobster"
+
+#' Raster data to display the Lobster Fishery Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Lobster_Fishery")), layer=1, values=TRUE)
+"Icon_Lobster_Fishery"
+
+#' Raster data to display the Predator Fisheries Icon for Tuna, Haddock, and Flatfish 
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Predator_Fisheries_Tuna_Haddock_Flatfish")), layer=1, values=TRUE)
+"Icon_Predator_Fisheries_Tuna_Haddock_Flatfish"
+
+#' Raster data to display the Predator Species Icon for Tuna, Haddock, and Flatfish 
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Predator_Species_Tuna_Haddock_Flatfish")), layer=1, values=TRUE)
+"Icon_Predator_Species_Tuna_Haddock_Flatfish"
+
+#' Raster data to display the Primary Production Icon
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Primary_Production")), layer=1, values=TRUE)
+"Icon_Primary_Production"
+
+#' Raster data to display the Protected Species Icon  
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Protected_Species")), layer=1, values=TRUE)
+"Icon_Protected_Species"
+
+#' Raster data to display the Protected Species and Tourism Icon  
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Protected_Species_and_Tourism_and_Tourism")), layer=1, values=TRUE)
+"Icon_Protected_Species_and_Tourism"
+
+#' Raster data to display the Tourism Icon  
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Tourism")), layer=1, values=TRUE)
+"Icon_Tourism"
+
+#' Raster data to display the Tuna Icon  
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Tuna")), layer=1, values=TRUE)
+"Icon_Tuna"
+
+#' Raster data to display the Tuna Fishery Icon  
+#' 
+#' @docType data
+# #' @usage raster(eval(parse(text = "Icon_Tuna_Fishery")), layer=1, values=TRUE)
+"Icon_Tuna_Fishery"
+

7615583d59a0d75df2ecf4d6bf9b6e874a8c34ef
ahart1 <arhart555@gmail.com>
2021-03-10 20:58:12 -0500

    Change example so resulting .png by default stored in current working directory.

--- R/GeneralDecisionTableFunction.R
+++ R/GeneralDecisionTableFunction.R
@@ -97,14 +97,14 @@
 #'
 #' # Decision table with default graphic settings, custom title and headers
 #' makeDecisionTable(data = data_df, OutputFileName = "Example1",
-#'                   OutputDirectory = tempdir(),
+#'                   OutputDirectory = getwd(),
 #'                   GraphicTitle = "Example decision table with default formatting",
 #'                   RowHeader = "Row header \n describes type \n of data in rows",
 #'                   ColumnHeader = "Column header describes type of data in columns")
 #' # Decision table with ranked coloring scheme, darker colors correspond with
 #' # better (larger) values in each row
 #' makeDecisionTable(data = data_df, OutputFileName = "Example2",
-#'                   OutputDirectory = tempdir(),
+#'                   OutputDirectory = getwd(),
 #'                   GraphicTitle = "Example decision table with ranked performance",
 #'                   RowHeader = "Row header \n describes type \n of data in rows",
 #'                   ColumnHeader = "Column header describes type of data in columns",
@@ -112,7 +112,7 @@
 #'                   barColors = "defaultRankColor", visualRank = "TRUE")
 #' # Decision table with summary row showing sum values for each column, single custom color.
 #' makeDecisionTable(data = data_df, OutputFileName = "Example3",
-#'                   OutputDirectory = tempdir(),
+#'                   OutputDirectory = getwd(),
 #'                   GraphicTitle = "Example decision table with summary row",
 #'                   RowHeader = "Row header \n describes type \n of data in rows",
 #'                   ColumnHeader = "Column header describes type of data in columns",
